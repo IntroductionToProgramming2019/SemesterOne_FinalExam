37638f4b643b97da37e8fc60a8617736dacc7709
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 16:40:27 +0000

    Add project files.

---
+++ big exam.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input euro symbol
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //create string do you want to buy another (acronymed) to use sentinal values to control the while loop, y means yes and keeps loop active, n means no and ends the loop
+            string dywtba = "y";
+            //dywtba must be y to stay in the loop, after it goes through the loop 
+            //each time it asks you to reassign dywtba to see whether to keep in the loop or end the program
+            while (dywtba == "y")
+            {
+                //asks for and assigns name and age to be used in ticket price calculation
+                Console.WriteLine("Please enter name : ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter age : ");
+                int age = int.Parse(Console.ReadLine());
+                //create a cost integer and calls a method that does the actual calculation of how much the ticket will be
+                //assigned values name and age are passed down as arguements to be used in teh calculation
+                int cost = CostOFTicket(name, age);
+                //costs are displayed and teh use is asked whether they want to buy another
+                Console.WriteLine("{0} the cost of your ticket is {1:c}", name, cost);
+                Console.WriteLine("Do you wish to buy another (y / n) : ");
+                dywtba = Console.ReadLine();
+            }
+        }
+        static int CostOFTicket(string name, int age)
+        {
+            int cost = 0;
+            if (age < 18)
+            {
+                cost = 10;
+            }
+            if ((age >= 18) && (age <= 21))
+            {
+                cost = 15;
+            }
+            else
+            {
+                cost = 20;
+            }
+            //the method put the user into a cost bracket depending on their age using if statements. the cost is then returned as an int
+            return cost;
+        }
+    }
+}
---
+++ final sem1 assessment/Program.cs
@@ -0,0 +1,129 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : Year One Semester One Final Exam
+ * 
+ * ############################# */
+
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_sem1_assessment
+{
+    class Program
+    {
+        static string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        static int[] otHoursPerDay = new int[7];
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // ask for the name of the user
+            Console.Write("Please enter the employee's name >>");
+            string name = Console.ReadLine();
+            //refer to this method to print the first heading 
+            PrintHeading(name);
+            //refer to the method to ask the user to enter values for each each for the number of hours they worked
+            EnterOvertime(name);
+            //spacing
+            Console.WriteLine();
+            //header
+            Console.WriteLine("========Printing Overtime===========");
+            //refers to the method that reads in and prints out all the overtime hours asked for by the method "EnterOvertime();"
+            PrintOvertime(name);
+            //spacing
+            Console.WriteLine();
+            //stores the value calculated in " CalculateTotalOvertime();" as a double for use in later calculations. if it were stored as an int this would result in inaccurate calculations
+            double totalOverTime = CalculateTotalOvertime();
+            Console.WriteLine("Total Overtime: {0} hours", totalOverTime);
+            Console.WriteLine();
+            double averageOt = CalculateAverageOvertime(totalOverTime);
+            Console.WriteLine("Average Daily Overtime: {0:0.00} hours", averageOt);
+            Console.WriteLine();
+            Console.WriteLine("========= Get Overtime For Specific Day ====");
+            Console.Write("Please enter a day>>");
+            string day = Console.ReadLine();
+            int getspecificday = GetOTForDay(day);
+            Console.WriteLine("Overtime for {0} is {1} hour(s)", day, getspecificday);
+            Console.WriteLine();
+            Console.WriteLine("========== Calculate Overtime Bill =======");
+            Console.Write("Please enter an hourly rate >> ");
+            double rate = double.Parse(Console.ReadLine());
+            double bill = CalculateOverTimeBill(rate, totalOverTime);
+            Console.WriteLine("Overtime bill for {0} is {1:C}", name, bill);
+
+
+
+        }
+        static void PrintHeading(string name)
+        {
+            string nameOfWorker = name;
+            Console.WriteLine("========== Overtime for {0} =========", name);
+            Console.WriteLine("========== Entering overtime hours =========");
+        }
+        static void EnterOvertime(string name)
+        {
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                Console.Write("Enter the number of overtime hrs for {0} >> ", daysOfWeek[i]);
+                otHoursPerDay[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void PrintOvertime(string name)
+        {
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                if (otHoursPerDay[i] > 1)
+                {
+                    Console.WriteLine("Overtime hours for {0} is {1} hours", daysOfWeek[i], otHoursPerDay[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Overtime hours for {0} is {1} hour", daysOfWeek[i], otHoursPerDay[i]);
+                }
+            }
+        }
+        static double CalculateTotalOvertime()
+        {
+            Console.WriteLine("========== Calculating Total Overtime =======");
+            double total = 0;
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                total = total + otHoursPerDay[i];
+            }
+            return total;
+
+        }
+        static double CalculateAverageOvertime(double totalOverTime)
+        {
+            Console.WriteLine("========== Calculating Average Overtime ===== ");
+            double average = totalOverTime / daysOfWeek.Length;
+            return average;
+        }
+        static int GetOTForDay(string day)
+        {
+            int answer = 0;
+            string specificDay = day;
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                if (specificDay == daysOfWeek[i])
+                {
+                    answer = otHoursPerDay[i];
+                }
+                else
+                {
+
+                }
+            }
+            return answer;
+        }
+        static double CalculateOverTimeBill(double rate, double totalOverTime)
+        {
+            double overtimeBill = rate * totalOverTime;
+            return overtimeBill;
+        }
+    }
+}
